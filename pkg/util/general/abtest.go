a5e9231836262965ffe49602d3e115412111c385
lihonghao.314 <lihonghao.314@bytedance.com>
2024-12-16 18:39:32 +0800

    support abtest, restrict indicator

---
+++ pkg/util/general/abtest.go
@@ -0,0 +1,33 @@
+package general
+
+import (
+	"hash/crc32"
+	"os"
+)
+
+const (
+	ABEnabledKey = "AB_SAMPLING_ENABLED"
+	ABEnabledVal = "true"
+)
+
+func EnableBorwein() bool {
+	mod := ABTestMod()
+	Infof("Node mod is %v", mod)
+	if mod == -1 {
+		return true
+	}
+	if mod < 80 {
+		return true
+	}
+	return false
+}
+
+func ABTestMod() int {
+	str := os.Getenv(ABEnabledKey)
+	if str != ABEnabledVal {
+		return -1
+	}
+	hostname, _ := os.Hostname()
+	hash := crc32.ChecksumIEEE([]byte(hostname))
+	return int(hash) % 100
+}
